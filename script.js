102246a552a070c509bd6853d6c1f9d49a3014cf
Bohdan Vakaliuk <54852515+BoleanSwift@users.noreply.github.com>
2020-09-04 15:33:09 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-//to the div
-
-document.querySelector('button').onclick = myClick;
-function myClick() {
-    let myText = document.querySelector('.i-1').value;
-    document.querySelector('.out').innerHTML = myText;
-}
